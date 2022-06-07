d6da2a854ebddf04ab16ef4137d4653c655894aa
mauro-december <mauro.botti@decemberlabs.com>
2022-06-07 19:25:57 -0300

    merge conflict

--- test.py
+++ test.py
@@ -6,15 +6,20 @@ from mpl_toolkits.axes_grid1 import Divider, Size
 import numpy as np
 
 def get_triangle(P):
+    print("Parametro P:", P)
     m = P * 0.5
     n = P * 0.5 + np.array([0.5, 0])
     k = P * 0.5 + np.array([0.25, np.sqrt(3)/4])
+    print("Variable M ", m)
+    print("Variable N ", n)
+    print("Variable K ", k)
     return np.array([m,n,k])
 
+print(np.sqrt(3)*5/2)
 # triangulo unitario inicial
 triangle = np.array([[0, 0],
-              [1, 0],
-              [0.5, np.sqrt(3)/2]])
+              [5, 0],
+              [2.5, np.sqrt(3)*5/2]])
 
 # Crea un array que representa el triangulo Sierpinski hasta la etapa deseada
 etapa = 2
@@ -24,6 +29,7 @@ for e in range(etapa):
 # Se procede a graficar el triangulo
 fig1 = plt.figure(facecolor='white')
 ax1 = fig1.add_subplot(111, aspect='equal')
+# ax1 = fig1.add_subplot(111, aspect='equal', facecolor='green')
 
 for t in triangle.reshape(3**etapa,3,2):
     ax1.add_patch(mpatches.Polygon(t, fc="y"))
