6a7b14f3bd879a1dc77968708cb5532bd5039268
Mauro Botti <33839339+MauroBW@users.noreply.github.com>
2022-07-04 00:53:34 -0300

    falta documentar lógica

--- triangulo-iterativo.py
+++ triangulo-iterativo.py
@@ -5,9 +5,9 @@ def main():
     win = GraphWin("Antena Fractal", 700, 900)
     dif = 71.9872981077807
 
-    antena_1 = Rectangle(Point(100,100 + dif), Point(350,350 + dif))
-    antena_2 = Rectangle(Point(355,100 + dif), Point(605,350 + dif))
-    palito = Rectangle(Point(351.5,100 + dif), Point(351.5,352 + dif))
+    antena_1 = Rectangle(Point(80,100 + dif), Point(330,350 + dif))
+    antena_2 = Rectangle(Point(370,100 + dif), Point(620,350 + dif))
+    palito = Rectangle(Point(339.5,100 + dif), Point(360,352 + dif))
     
     antena_1.setFill("black")
     antena_2.setFill("black")
@@ -17,19 +17,11 @@ def main():
     antena_2.draw(win)
     palito.draw(win)
 
-
-
     altura = (((489)*math.sqrt(3))/2)
-    print("Altura del triangulo:", altura)
-
-    
-    print(500-((((500)*math.sqrt(3))/2)))
 
     A = Point(100,800)
     B = Point(600,800)
-    C = Point(353, altura)
-
-    
+    C = Point(350, altura)
 
     def getSubTriangulos(pointA, pointB, pointC):
         triangulos_procesar = []
@@ -65,6 +57,7 @@ def main():
         for n in range(int(resp)):
             suma += (3 ** n)
 
+        print("Esfuerzo:", suma)
         return suma
 
     def sierpinski():
