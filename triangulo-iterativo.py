b6c4d275832a013bdb0493691cb4291e57ed44b2
MauroBW <maurobotti99@gmail.com>
2022-07-04 15:49:54 -0300

    comentarios sobre metodos

--- triangulo-iterativo.py
+++ triangulo-iterativo.py
@@ -4,8 +4,9 @@ import math
 def main():
     win = GraphWin("Antena Fractal", 700, 900)
     win.setBackground("white")
-    dif = 71.9872981077807
+    dif = 71.9872981077807 # Diferencia en coordenadas Y para obtener triangulo conextado a palito
 
+    # Figuras estáticas
     antena_1 = Rectangle(Point(80,100 + dif), Point(330,350 + dif))
     antena_2 = Rectangle(Point(370,100 + dif), Point(620,350 + dif))
     palito = Rectangle(Point(339.5,100 + dif), Point(360,352 + dif))
@@ -18,12 +19,14 @@ def main():
     antena_2.draw(win)
     palito.draw(win)
 
-    altura = (((489)*math.sqrt(3))/2)
+    altura = (((489)*math.sqrt(3))/2) # Calculo de la altura del triangulo
 
+    # Coordenadas Triangulo inicial
     A = Point(100,800)
     B = Point(600,800)
     C = Point(350, altura)
 
+    # Operación para obtener sub_triangulos a partir de coordenadas dadas por parametro
     def getSubTriangulos(pointA, pointB, pointC):
         triangulos_procesar = []
         
@@ -35,24 +38,25 @@ def main():
         sub_triangle_2 = Polygon(pointB, AB, BC)
         sub_triangle_3 = Polygon(pointC, CA, BC)
 
+        triangulos_procesar.append(sub_triangle_3)
         triangulos_procesar.append(sub_triangle_1)
         triangulos_procesar.append(sub_triangle_2)
-        triangulos_procesar.append(sub_triangle_3)
 
         return triangulos_procesar
 
+    # Operación para obtener puntos medios dados 2 puntos
     def operacionCoord(point1, point2):
         x = (point1.getX() / 2 + point2.getX() / 2)
         y = (point1.getY() / 2 + point2.getY() / 2)
         return Point(x,y)
 
+    # Dibujado de triangulos
     def procesar_triangulos(a_procesar):
-
         for triangle in a_procesar:
             triangle.setFill("black")
             triangle.draw(win)
     
-    # Se chequea que el input sea numero positivo
+    # Validación para el input generador
     def check():
         resp = input("Iteracion Triangulo Sierpinski: ")
         if (resp.isnumeric() == False):
@@ -65,6 +69,7 @@ def main():
             else:
                 sierpinski(esfuerzo(resp))    
 
+    # Cantidad de triangulos a procesar
     def esfuerzo(resp):
         suma = 0
         for n in range(int(resp)):
@@ -73,6 +78,7 @@ def main():
         print("Esfuerzo:", suma)
         return suma
 
+    # Armado del triangulo a partir de esfuerzo indicado
     def sierpinski(obj):
         triangulo_mayor = Polygon(A,B,C)
         aux_triangulos = [triangulo_mayor]
@@ -85,8 +91,7 @@ def main():
                 aux_triangulos.append(triangulo)
         procesar_triangulos(aux_triangulos)
 
-        print("Antena representada correctamente!")
-        input("Presiona la tecla enter para salir.")
+        input("Antena representada con exito!\nPresiona la tecla enter para salir.")
         quit()
 
     check()
